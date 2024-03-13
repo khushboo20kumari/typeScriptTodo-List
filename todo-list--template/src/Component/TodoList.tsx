d31c282da72e774b514b0a5aa77c8b8415af9cf7
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-03-13 23:23:55 +0530

    Completed Delete button

--- todo-list--template/src/Component/TodoList.tsx
+++ todo-list--template/src/Component/TodoList.tsx
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 const TodoList: React.FC = () => {
-   
+
+    const [item, setItem] = useState<string>("")
+    const [todos, setTodos] = useState<string[]>([]);
+
+    const HandlerOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setItem(event.target.value)
+    }
+
+    const OnClickHandler = () => {
+        setTodos([...todos, item])
+        setItem("")
+    }
+
+
+
+    const OnDelete = (index: number) => {
+        setTodos(todos.filter((_, i) => i !== index));
+    };
     return (
         <>
-            <p>i am khushbooo</p>
+            <p> Todo List</p>
+            <input onChange={HandlerOnChange} value={item}></input>
+            <button onClick={OnClickHandler}>add</button>
+            {todos.map((ele, index) => (
+                <>
+                    <p>{ele}</p>
+                    <button onClick={()=>OnDelete(index)}>Delete</button>
+                </>
+            ))}
+
         </>
-    );
-};
+
+    )
+}
 export default TodoList;
+
+
