83ace3ef91615311e80a2d963eca4a795af5d9f1
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-03-13 14:00:02 +0530

    created component

---
+++ todo-list--template/src/Component/TodoList.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+const TodoList: React.FC = () => {
+   
+    return (
+        <>
+            <p>i am khushbooo</p>
+        </>
+    );
+};
+export default TodoList;
