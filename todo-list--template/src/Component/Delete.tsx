bd982d5a868a1a9a565c2cfc5d7ffa15c1de76f4
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-03-15 17:16:52 +0530

    todo list style change

---
+++ todo-list--template/src/Component/Delete.tsx
@@ -0,0 +1,7 @@
+const Delete: React.FC = (OnDelete) => {
+    return (
+        <>
+            <button onClick={() => OnDelete(index)}>Delete</button>
+        </>
+    )
+}
--- todo-list--template/src/Component/TodoList.tsx
+++ todo-list--template/src/Component/TodoList.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 const TodoList: React.FC = () => {
 
     const [item, setItem] = useState<string>("")
@@ -12,28 +17,52 @@ const TodoList: React.FC = () => {
         setTodos([...todos, item])
         setItem("")
     }
-
-
-
     const OnDelete = (index: number) => {
         setTodos(todos.filter((_, i) => i !== index));
     };
+
     return (
         <>
-            <p> Todo List</p>
-            <input onChange={HandlerOnChange} value={item}></input>
-            <button onClick={OnClickHandler}>add</button>
-            {todos.map((ele, index) => (
-                <>
-                    <p>{ele}</p>
-                    <button onClick={()=>OnDelete(index)}>Delete</button>
-                </>
-            ))}
-
+            <center>
+                <Card sx={{ maxWidth: 500 }}>
+                    <CardContent>
+                        <p className='title'>Todo List</p>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={HandlerOnChange} value={item} />
+                        <Button variant="contained" onClick={OnClickHandler}>Add</Button>
+                        <div className='todoList'>
+                            {todos.map((ele, index) => (
+                                <div key={index} className='todoItem'>
+                                    <p>{ele}</p>
+                                    <button onClick={() => OnDelete(index)}>Delete</button>
+                                </div>
+                            ))}
+                        </div>
+                    </CardContent>
+                </Card>
+            </center>
         </>
-
     )
 }
 export default TodoList;
 
 
+
+
+// import * as React from 'react';
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
+// function ImgMediaCard() {
+//     return (
+//         <center>
+//             <Card sx={{ maxWidth: 345 }}>
+
+//                 <CardContent>
+
+//                 </CardContent>
+
+//             </Card>
+//         </center>
+//     );
+// }
+
+// export default ImgMediaCard;
